182bb5206cf9a453ffc40041017204a77bc6b93a
K1715 <K1715@ICT-421-WS-26>
2017-01-31 14:30:43 +0200

    testi tehtävä

---
+++ Viikkotehtavat2/Kiuas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat2
+{
+    class Heater
+    {
+        public bool IsOn { get; set; }
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+    }
+}
---
+++ Viikkotehtavat2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create one heater object
+            Heater heater = new Heater();
+            // set some values
+            heater.IsOn = true;
+            heater.Temperature = 100;
+            heater.Humidity = 78;
+            // print values
+            Console.WriteLine("Is heater on : {0}", heater.IsOn);
+            Console.WriteLine("Heater temperature : {0}", heater.Temperature);
+            Console.WriteLine("Heater humidity : {0}", heater.Humidity);
+
+            // changes values
+            heater.Temperature = 0;
+            heater.Humidity = 0;
+            heater.IsOn = false;
+            // print values
+            Console.WriteLine("Is heater on : {0}", heater.IsOn);
+            Console.WriteLine("Heater temperature : {0}", heater.Temperature);
+            Console.WriteLine("Heater humidity : {0}", heater.Humidity);
+        }
+    }
+}
