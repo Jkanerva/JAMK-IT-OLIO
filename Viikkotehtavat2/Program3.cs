cb0c665ea45ada52534ed9dfb3fea7e050bcd253
K1715 <K1715@ICT-421-WS-26>
2017-01-31 17:24:32 +0200

    harjoitukset

---
+++ Viikkotehtavat2/Program3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat2
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            // TV
+            TV tv = new TV();
+            tv.Merkki = "Samsung";
+            tv.IsOpen = false;
+            Console.WriteLine(tv.ToString());
+
+            // tv auki
+            tv.IsOpen = true;
+            tv.Kanava = 4;
+            tv.Ääni = 10;
+            Console.WriteLine(tv.ToString());
+
+            // tv kiinni
+            tv.IsOpen = false;
+            Console.WriteLine(tv.ToString());
+        }
+    }
+}
---
+++ Viikkotehtavat2/TV.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat2
+{
+    class TV
+    {
+        public string Merkki { get; set; }
+        private bool isOpen;
+        public bool IsOpen
+        {
+            get
+            {
+                return isOpen;
+            }
+            set
+            {
+                isOpen = value;
+                if (value == false)
+                {
+                    Kanava = 0;
+                    Ääni = 0;
+                }
+            }
+        }
+        public int Kanava { get; set; }
+        public int Ääni { get; set; }
+        public override string ToString()
+        {
+            return " Merkki " + Merkki + " Kanava " + Kanava + " Ääni " + Ääni;
+        } 
+    }
+}
