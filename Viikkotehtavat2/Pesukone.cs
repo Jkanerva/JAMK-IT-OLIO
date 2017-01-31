448b2e22dd921002d804e151f62b75b88700f327
K1715 <K1715@ICT-421-WS-26>
2017-01-31 14:53:14 +0200

    Testi tehtävä2

---
+++ Viikkotehtavat2/Pesukone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat2
+{
+    class Pesukone
+    {
+        public string Merkki { get; set; }
+        public int PyörintäNopeus { get; set; }
+        public Pesukone()
+        {
+
+        }
+        public Pesukone(string merkki)
+        {
+            Merkki = merkki;
+        }
+        public override string ToString()
+        {
+            return " Merkki: " + Merkki + " PyörintäNopeus: " + PyörintäNopeus;
+        }
+    }
+}
---
+++ Viikkotehtavat2/Program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            // Pesukone
+            Pesukone pesukone = new Pesukone();
+            pesukone.Merkki = "Samsung";
+            pesukone.PyörintäNopeus = 2000;
+            Console.WriteLine(pesukone.ToString());
+            Console.ReadLine();
+        }
+    }
+}
