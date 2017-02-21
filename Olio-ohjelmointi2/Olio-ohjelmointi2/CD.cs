457eddadd33c1d6bd12ca3ab0cf83bea39bde86e
Jimi <Jimi@DESKTOP-6C3B491>
2017-02-21 13:15:35 +0200

    valmis

---
+++ Olio-ohjelmointi2/Olio-ohjelmointi2/CD.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_ohjelmointi2
+{
+    class CD
+    {
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public List<Song> Songs;
+
+        public CD()
+        {
+            Songs = new List<Song>();
+        }
+        public void AddSong(Song song)
+        {
+            Songs.Add(song);
+        }
+        public override string ToString()
+        {
+            string data = "CD: ";
+            data += "\n-Artist: " + Artist + "\n-Name: " + Name + "\n-Genre: " + Genre;
+            data += "\nSongs:";
+            int totalLength = 0;
+            foreach (Song song in Songs)
+            {
+                if (song != null)
+                {
+                    data += "\n--" + song.ToString();
+                    totalLength += song.LengthInSeconds;
+                }
+            }
+            int min = totalLength / 60;
+            int sec = totalLength % 60;
+            string duration = min.ToString("00") + ":" + sec.ToString("00");
+            data += "\n-Total duration: " + duration;
+            return data;
+        }
+    }
+}
---
+++ Olio-ohjelmointi2/Olio-ohjelmointi2/Program7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_ohjelmointi2
+{
+    class Program7
+    {
+        static void Main(string[] args)
+        {
+            // CD
+            CD cd = new CD();
+            cd.Artist = "Popeda";
+            cd.Name = "Ukkometso";
+            cd.Genre = "Rock";
+
+            cd.AddSong(new Song { Name = "Elän itselleni", LengthInSeconds = 428 });
+            cd.AddSong(new Song { Name = "Mä elän vieläkin", LengthInSeconds = 303 });
+            cd.AddSong(new Song { Name = "Kellot Lyö", LengthInSeconds = 359 });
+
+            Console.WriteLine(cd.ToString());
+            Console.WriteLine();
+        }
+    }
+}
