43c376f5a93675f2b74e0b0de10f48f28c4cef35
Jimi <Jimi@DESKTOP-6C3B491>
2017-03-07 13:21:56 +0200

    tekstitiedosto

---
+++ Olio-ohjelmointi3/Olio-ohjelmointi3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Olio_ohjelmointi3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write
+            string line;
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Public\WriteLines2.txt"))
+            {
+                do
+                {
+                    Console.Write("Give a text line (enter ends) : ");
+                    line = Console.ReadLine();
+                    if (line.Length != 0) sw.WriteLine(line);
+                } while (line.Length != 0);
+            }
+
+            //Read
+            using (StreamReader sr = new StreamReader(@"C:\Users\Public\WriteLines2.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
