24bbb2fa41381fdf0807eb740e725280eda1d66e
K1715 <K1715@ICT-421-WS-26>
2017-03-21 17:28:17 +0200

    tehty

---
+++ BlackJackV0/TehtäviäTests/CalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoL08;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoL08.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calc = new Calculator();
+            int n1 = 5;
+            int n2 = 8;
+            int expected = 13;
+            int actual = calc.Add(n1, n2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Tehtäviä/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtäviä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"D:\Temp\nimet.txt");
+            System.Console.WriteLine("Contents of nimet.txt = {0}", text);
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
+
