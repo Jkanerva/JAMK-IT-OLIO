265ac5ad1875dd65ff4cd16b6b838323a844e712
Jimi <Jimi@DESKTOP-6C3B491>
2017-04-04 13:38:10 +0300

    kesken

---
+++ Olio-ohjelmointi5/H6/Kiuas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H6
+{
+    class kiuas
+    {
+        float temp = 0f;
+        float humid = 0f;
+
+        public float Temp { get { return temp; } }
+        public float Humid { get { return humid; } }
+
+        public void SetTemp(float f)
+        {
+            if (f >= 0 && f <= 120)
+            {
+                temp = f;
+            }
+        }
+        public void SetHumid(float f)
+        {
+            if (f >= 0 && f <= 100)
+            {
+                humid = f;
+            }
+        }
+    }
+}
---
+++ Olio-ohjelmointi5/H6/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        kiuas Kiuas;
+        string s = "";
+        double temp, humi;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void customClick(object sender, RoutedEventArgs e)
+        {
+            s += sender.ToString().Substring(sender.ToString().Length - 1, 1);
+            txtResult.Text = s;
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            if (rdbHumidity.IsChecked == true)
+            {
+                float f;
+                float.TryParse(s, out f);
+                Kiuas.SetHumid(f);
+                lblHumidity.Content = Kiuas.Humid;
+
+            }
+            else if (rdbTemperature.IsChecked == true)
+            {
+                float f;
+                float.TryParse(s, out f);
+                Kiuas.SetTemp(f);
+                lblTemperature.Content = Kiuas.Temp;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Kiuas = new kiuas();
+        }
+
+        private void btnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+            s = txtResult.Text.Substring(0, txtResult.Text.Length - 1);
+            txtResult.Text = s;
+        }
+    }
+}
