e5c3277421ef9bd1b29b91719223ca3f3093d5ef
Jimi <Jimi@DESKTOP-6C3B491>
2017-04-04 13:22:56 +0300

    ei pelaa

---
+++ Olio-ohjelmointi5/H5/Lotto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H5
+{
+    class Lotto
+    {
+        private Random rand = new Random();
+
+        public List<int> getRandomNumbers(string type)
+        {
+ 
+            int largestNumber = 39;
+            int numberAmount = 7;
+
+            if (type.Equals("Lotto"))
+            {
+                largestNumber = 39;
+                numberAmount = 7;
+            }
+            else if (type.Equals("Viking Lotto"))
+            {
+                largestNumber = 48;
+                numberAmount = 6;
+            }
+            else if (type.Equals("Eurojackpot"))
+            {
+                largestNumber = 50;
+                numberAmount = 5;
+            }
+
+            List<int> numbers = new List<int>();
+            do
+            {
+                int number = rand.Next(1, largestNumber + 1);
+                if (!numbers.Contains(number)) numbers.Add(number);
+            } while (numbers.Count < numberAmount);
+
+            numbers.Sort();
+
+            if (type.Equals("Eurojackpot"))
+            {
+                List<int> starNumbers = new List<int>();
+                do
+                {
+                    int number = rand.Next(1, 11);
+                    if (!starNumbers.Contains(number)) starNumbers.Add(number);
+                } while (starNumbers.Count < 2);
+
+                starNumbers.Sort();
+                numbers.AddRange(starNumbers);
+            }
+            return numbers;
+        }
+    }
+}
---
+++ Olio-ohjelmointi5/H5/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Lotto lotto;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            lotto = new Lotto();
+        }
+
+        private void drawButton_Click(object sender, RoutedEventArgs e)
+        {
+            int rows;
+            bool result = int.TryParse(rowsTextBox.Text, out rows);
+            if (result)
+            {
+                if (rows > 0) RandomizeNumbers(rows);
+                else
+                {
+                    Debug.WriteLine("Amount of lottery rows to draw needs to be a number!");
+                }
+            }
+            else
+            {
+                Debug.WriteLine("Amount of lottery rows to draw can't be negative!");
+            }
+        }
+
+        private void RandomizeNumbers(int rows)
+        {
+            string numbersString = "";
+            for (int i = 1; i <= rows; i++)
+            {
+                // get random numbers by selected lottery type
+                List<int> numbers = lotto.getRandomNumbers(lottoComboBox.SelectedValue.ToString());
+                numbersString += "Row " + i + ": ";
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    numbersString += numbers[j] + " ";
+                }
+                numbersString += Environment.NewLine;
+            }
+            numbersTextBlock.Text = numbersString;
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            numbersTextBlock.Text = "";
+        }
+    }
+}
